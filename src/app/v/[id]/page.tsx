ded51f9dde615189a14c49d889a1c20a33132d60
Tomas Flynn <tomaseflynn@gmail.com>
2026-03-09 16:50:35 -0300

    fix: movimos la linea de use client

--- src/app/v/[id]/page.tsx
+++ src/app/v/[id]/page.tsx
@@ -1,7 +1,8 @@
+"use client";
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
-"use client";
+
 import { useState, useEffect } from 'react';
 import invitadosData from '@/data/invitados.json';
 
